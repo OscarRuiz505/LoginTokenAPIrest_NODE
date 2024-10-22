e3b6c1bd7b0bd2d700d9f344020fb9f1f775a835
OscarRuiz505 <oscarruizm31@gmail.com>
2024-10-22 17:08:36 -0500

    Modulo Dos: Gestor de productos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const productosRoutes = require('./routes/productosRoutes');
 
 // Conectar a la base de datos
 conectarDB();
@@ -19,6 +20,9 @@ app.get('/', (req, res) => {
 
 // Usar rutas de autenticación
 app.use('/api/auth', authRoutes);
+
+// Rutas de productos
+app.use('/api', productosRoutes); // Usar rutas de productos
 
 // Configurar el puerto en el que escuchará el servidor
 const PORT = 5000;
